8563d8778d9a301fea34ff04bda37a29b1c340a2
Raycia <raycialima@gmail.com>
2023-05-27 13:57:56 -0300

    ajuste de testes

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,18 @@ test('teste titulo', () => {
   const testCase = document.querySelector('.App h1');
   expect(testCase).toHaveTextContent(/meu painel de controle/i);
 });
+
+
+test('teste titulo 2'), () => {
+  render(<App />);
+  const testCase = document.querySelector('.App h1');
+  expect(testCase).toHaveTextContent(/teste erro/i);
+});
+
+
+test('teste botao', () => {
+  render(<App />);
+  const button = screen.getByText('Clique aqui');
+  expect(button).toBeInTheDocument();
+});
+
